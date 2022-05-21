02352133fed65693d24a0fc2ea8295c3a7fe86b2
runcloud <runcloud@sunth.onmicrosoft.com>
2022-05-21 08:39:37 +0800

    third commit

--- src/App.svelte
+++ src/App.svelte
@@ -1,13 +1,30 @@
 <script>
+	import Example2 from "./Example2.svelte";
 	export let name;
+
+	var today = new Date();
+	var ny = new Date(2023, 0, 1); //1st January 2023
+	var one_day = 1000 * 60 * 60 * 24; //number of millisecond in 1 day
+
+	var result = Math.round((ny - today) / one_day);
+
+	alert("There are " + result + " days left until New Year");
 </script>
 
 <main>
 	<div>
-		<label>Name: <input type="text" name="UserName" bind:value="{name}" /></label>
+		<label>
+			Name: <input type="text" name="UserName" bind:value={name} />
+		</label>
 	</div>
 	<h1>Hello {name}!</h1>
-	<p>Visit the <a href="https://svelte.dev/tutorial">Svelte tutorial</a> to learn how to build Svelte apps.</p>
+	<p>
+		Visit the <a href="https://svelte.dev/tutorial">Svelte tutorial</a> to learn
+		how to build Svelte apps.
+	</p>
+	<p>There are {result} days left until New Year</p>
+	<hr />
+	<Example2 str="{name}" />
 </main>
 
 <style>
---
+++ src/Example2.svelte
@@ -0,0 +1,29 @@
+<script>
+    export var str = "i have learned something new today";
+    function capitalizeFirstCharOfEachWord() {
+        var strArray = str.split(" ");
+        var i;
+        for (i = 0; i < strArray.length; i++) {
+            strArray[i] =
+                strArray[i].charAt(0).toUpperCase() + strArray[i].slice(1); //Capitalize the first character and join with the remain characters of the word
+        }
+        str = strArray.join(" ");
+    }
+</script>
+
+<main>
+    <div>
+        <h1>
+            Example 2 - Capitalize the 1st character of each word in a given
+            string.
+        </h1>
+        <p>
+            <button on:click={capitalizeFirstCharOfEachWord}>
+                Capitalize the 1st character of each word
+            </button>
+        </p>
+        <p>
+            {str}
+        </p>
+    </div>
+</main>
